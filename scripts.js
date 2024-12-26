d1095fbc74d733479047ed183bd56329a8c32cbf
101113608 <48575130+101113608@users.noreply.github.com>
2024-12-26 15:30:18 +1100

    Add CardElement constructor
    To group up all of the HTML elements that are used to display the book
    to the user, the CardElement constructor was added to scripts.js.

--- scripts.js
+++ scripts.js
@@ -28,6 +28,51 @@ Book.prototype = {
     }
 }
 
+function CardElement(book, index) {
+    function createBookDataDiv() {
+        let bookData = {
+            containerDiv: document.createElement("div"),
+            bookTitle: document.createElement("h2"),
+            bookAuthor: document.createElement("h4"),
+            bookPages: document.createElement("h4"),
+        }
+
+        bookData.bookTitle.textContent = book.title;
+        bookData.bookAuthor.textContent = book.author;
+        bookData.bookPages.textContent = `${book.pages} Pages`;
+
+        bookData.containerDiv.setAttribute("class", "book-data");
+        bookData.containerDiv.append(bookData.bookTitle, bookData.bookAuthor, bookData.bookPages);
+
+        return bookData;
+    }
+
+    function createBookStatusP() {
+        let bookStatus = {
+            containerP: document.createElement("p"),
+            containerSpan: document.createElement("span"),
+        }
+
+        bookStatus.containerSpan.textContent = book.getReadStatus();
+        bookStatus.containerSpan.setAttribute("class", `${myLibrary[index].hasRead ? "blue-text" : "red-text"}`);
+
+        bookStatus.containerP.textContent = "Status: ";
+        bookStatus.containerP.setAttribute("class", "book-read-status");
+        bookStatus.containerP.append(bookStatus.containerSpan);
+
+        return bookStatus;
+    }
+
+    this.containerDiv = document.createElement("div");
+    this.bookData = createBookDataDiv();
+    this.bookStatus = createBookStatusP();
+
+    this.containerDiv.setAttribute("class", "card")
+    this.containerDiv.setAttribute("data-index", index); // Object to DOM Node connection
+
+    this.containerDiv.append(this.bookData.containerDiv, this.bookStatus.containerP);
+}
+
 function addBookToLibrary(title, author, pages, hasRead) {
     let book = new Book(title, author, pages, hasRead);
     myLibrary.push(book);
