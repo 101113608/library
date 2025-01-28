626072c25601198e6745c4891b3dd24d15fa9e3b
101113608 <48575130+101113608@users.noreply.github.com>
2025-01-29 00:04:39 +1100

    Refactor Book constructor fn & prototype to class

--- scripts.js
+++ scripts.js
@@ -6,25 +6,25 @@ const cardElementArr = [];
 const modalElement = document.querySelector("dialog");
 const formElement = document.querySelector("form");
 
-function Book(title, author, pages, hasRead) {
-    this.title = title,
-        this.author = author,
-        this.pages = pages,
-        this.hasRead = hasRead;
-}
+class Book {
+    constructor(title, author, pages, hasRead) {
+        this.title = title,
+            this.author = author,
+            this.pages = pages,
+            this.hasRead = hasRead;
+    }
 
-Book.prototype = {
     getReadStatus() {
         if (this.hasRead) {
             return `Has read`;
         }
 
         return `Not yet read`;
-    },
+    }
 
     toggleReadStatus() {
         this.hasRead = !this.hasRead;
-    },
+    }
 
     getBookInfo() {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.bookRead()}`
